4602a37d1af51030c3bf78222f28d4ca826407b8
Sidsky <61586290+Sidsky@users.noreply.github.com>
2022-09-19 12:03:17 +0800

    Changes score to laptime

--- src/components/database.js
+++ src/components/database.js
@@ -2,43 +2,31 @@ export const Leaderboard = [
     {
         name: "MMC",
         location: "Hong Kong",
-        score : 6550,
-        img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-        dt: "2022-02-10"
+        score : "01:22:791",
     },
     {
         name: "Fidel Hand",
         location: "USA",
-        score : 2310,
-        img: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-        dt: "2021-01-01"
+        score : "01:23:789",
     },
-    // {
-    //     name: "Bessie Hickle",
-    //     location: "Chaina",
-    //     score : 350,
-    //     img: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-    //     dt: "2021-08-17"
-    // },
-    // {
-    //     name: "Adella Wunsch",
-    //     location: "Japan",
-    //     score : 2100,
-    //     img: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-    //     dt: "2022-09-14"
-    // },
-    // {
-    //     name: "Clair O'Connell",
-    //     location: "London",
-    //     score : 1250,
-    //     img: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-    //     dt: "2022-01-22"
-    // },
-    // {
-    //     name: "Kameron Prosacco",
-    //     location: "Canada",
-    //     score : 5250,
-    //     img: "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-    //     dt: "2022-01-21"
-    // }
+    {
+        name: "Bessie Hickle",
+        location: "Chaina",
+        score : "01:22:790",
+    },
+    {
+        name: "Adella Wunsch",
+        location: "Japan",
+        score : "02:22:789",
+    },
+    {
+        name: "Clair O'Connell",
+        location: "London",
+        score : "02:23:789",
+    },
+    {
+        name: "Kameron",
+        location: "Canada",
+        score : "00:23:787",
+    }
 ]
--- src/components/profile.js
+++ src/components/profile.js
@@ -1,5 +1,13 @@
 import React from "react";
 
+function importAll(r) {
+    let images = {};
+    r.keys().forEach((item, index) => { images[item.replace('./', '')] = r(item); });
+    return images
+}
+
+const images = importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
+
 export default function Profiles({Leaderboard}){
     return(
         <div id="profile">
@@ -16,6 +24,8 @@ function Item(data){
                     <div className="flex" key={index}>
                         <div className="item">
                             <img src={value.img} alt="" />
+                            {/*<img src={require('./images/1.png')} alt="" />*/}
+                            <img src={images[`${index+1}.png`]} className="numbers" alt=""/>>
                             <div className="info">
                                 <h3 className="name text-dark">{value.name}</h3>
                                 <span>{value.location}</span>
